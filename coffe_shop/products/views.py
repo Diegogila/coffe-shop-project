1870f064dbf49417778017c8ed068d06722cfdbc
diegogil <dgg26496@hotmail.com>
2024-10-11 17:57:30 -0600

    added models and products form

---
+++ coffe_shop/products/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import ProductForm
+
+# Create your views here.
+class ProductFormView(FormView):
+    template_name = 'products/add_product.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('add_product')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
